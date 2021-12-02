a0c7224c5ee7252f135be029a46df31611d0771e
JamisonFox <2616220@gmail.com>
2021-12-02 22:07:47 +0300

    second_commit

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,6 @@
+<h1>YO BRO WHO GOT U SMILING LIKE THAT LIKE</h1>
+<h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi fugit ipsa maxime molestiae nulla repellat, vel. Ex expedita possimus quod.</h2>
+<h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi fugit ipsa maxime molestiae nulla repellat, vel. Ex expedita possimus quod.</h2>
+<h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi fugit ipsa maxime molestiae nulla repellat, vel. Ex expedita possimus quod.</h2>
+<h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi fugit ipsa maxime molestiae nulla repellat, vel. Ex expedita possimus quod.</h2>
+<h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi fugit ipsa maxime molestiae nulla repellat, vel. Ex expedita possimus quod.</h2>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//ф-ции для вывода : dump(),dd() - выводит и убирает все остальное,ddd() - ultimate)
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('hello',[SiteController::class, 'index']);
